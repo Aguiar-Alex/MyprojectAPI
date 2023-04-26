bc225fd3133055fa077ddbe09f07ec89ad960300
Antonio <antonio.alex.aguiar@gmail.com>
2023-04-25 21:05:52 -0300

    Entity OrderProducts

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -4,9 +4,11 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import OrderProducts from './OrdersProducts';
 
 @Entity('Orders')
 export default class Order {
@@ -16,6 +18,11 @@ export default class Order {
   @ManyToOne(() => Customers)
   @JoinColumn({ name: 'customer_id' })
   customer: Customers;
+
+  @OneToMany(() => OrderProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrderProducts[];
 
   @CreateDateColumn()
   created_at: Date;
--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,7 +1,9 @@
+import OrderProducts from '@modules/orders/typeorm/entities/OrdersProducts';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -11,6 +13,10 @@ export default class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @OneToMany(() => OrderProducts, order_products => order_products.product)
+  order_products: OrderProducts[];
+
+  @CreateDateColumn()
   @Column()
   name: string;
 
