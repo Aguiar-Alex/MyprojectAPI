8d88234b3bf4b6794a8cc0f5085d76280fe4bfe2
Antonio <antonio.alex.aguiar@gmail.com>
2023-05-15 21:08:37 -0300

    Paginação modulo users

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -5,9 +5,12 @@ import DeleteUserService from '../services/DeleteUserService';
 
 export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
+    const page = request.query.page ? Number(request.query.page) : 1;
+    const limit = request.query.limit ? Number(request.query.limit) : 15;
+
     const listUsers = new ListUserService();
 
-    const users = await listUsers.execute();
+    const users = await listUsers.execute({ page, limit });
 
     return response.json(users);
   }
--- src/modules/users/services/ListUsersService.ts
+++ src/modules/users/services/ListUsersService.ts
@@ -1,12 +1,35 @@
 import { PostgresDataSource } from '@shared/http/typeorm/AppDataSource';
 import User from '../typeorm/entities/User';
 
+interface SearchParams {
+  page: number;
+  limit: number;
+}
+
+interface IPaginateUser {
+  per_page: number;
+  total: number;
+  current_page: number;
+  data: User[];
+}
+
 export default class ListUserService {
-  public async execute(): Promise<User[]> {
+  public async execute({ page, limit }: SearchParams): Promise<IPaginateUser> {
     const userRepository = PostgresDataSource.getRepository(User);
 
-    const users = userRepository.find();
+    const [users, count] = await userRepository
+      .createQueryBuilder('Users')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getManyAndCount();
 
-    return users;
+    const result = {
+      per_page: limit,
+      total: count,
+      current_page: page,
+      data: users,
+    };
+
+    return result as IPaginateUser;
   }
 }
